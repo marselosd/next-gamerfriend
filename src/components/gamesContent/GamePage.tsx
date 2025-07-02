f8043d95672f35299447683c4f548f263612a020
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2025-07-02 10:16:28 -0300

    done

---
+++ src/components/gamesContent/GamePage.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function GamePage() {
+    return(
+        <>
+        <div className="bg-white min-h-screen text-gray-800 px-4 py-6">
+           
+        </div>
+        </>
+    )
+}
