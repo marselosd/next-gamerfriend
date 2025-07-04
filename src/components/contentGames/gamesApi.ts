a346fb370ea2568a2036b5bbcdf81af40312f77d
Marcelo Dalcin <marcelo_s.dalcin@hotmail.com>
2025-07-04 09:44:59 -0300

    render

--- src/components/contentGames/gamesApi.ts
+++ src/components/contentGames/gamesApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const gamerFriendApi = createApi({
   reducerPath: 'gamerfriendApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://https://apigamefriends.onrender.com/", 
+    baseUrl: "https://apigamefriends.onrender.com/", 
     prepareHeaders: (headers) => {
     const token = localStorage.getItem('token');
     if (token) {
